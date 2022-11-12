15ccbf8ee33320ecdb2df4d60a94378a78d58d9b
J-DVbk <j-d.vbk@gmx.de>
2022-11-12 01:12:38 +0100

    Fixed and Iterating Gate Functionality

--- RoterBaer/Assets/Scripts/Tracks/TrackGate.cs
+++ RoterBaer/Assets/Scripts/Tracks/TrackGate.cs
@@ -1,12 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TrackGate : TrackPoint
 {
     [SerializeField] ETrackGateType gateType;
 
+    [SerializeField] TrackPoint nextExit;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (nextExit == null && gateType == ETrackGateType.Fixed)
+        {
+            Debug.LogError($"[{GetType().Name}] Fixed Gate '{gameObject.name}' with undefined Exit.", this);
+
+            if (neighbors.Count > 0) nextExit = neighbors.First();
+        }
+    }
+
+    public override TrackPoint GetNextNode(TrackPoint previousPoint)
+    {
+        if (!IsValidPreviousPoint(previousPoint))
+            return previousPoint;
+
+        switch (gateType)
+        {
+            case ETrackGateType.Iterating:
+                TrackPoint oldNextPoint = nextExit;
+                SetNextExit(countUp: true);
+                return oldNextPoint;
+
+            case ETrackGateType.Fixed:
+            case ETrackGateType.Player1:
+            case ETrackGateType.Player2:
+                break;
+
+            default:
+                Debug.Log($"[{GetType().Name}] {nameof(GetNextNode)} is UNDEFINED for {gateType}", this);
+                break;
+        }
+        return nextExit;
+    }
+
+    public void SetNextExit(bool countUp)
+    {
+        int nextID = 0;
+        for (int i = 0; i < neighbors.Count; i++)
+        {
+            if (neighbors[i] == nextExit)
+            {
+                nextID = i;
+                break;
+            }
+        }
+
+        nextID = (nextID + neighbors.Count + (countUp ? 1 : -1)) % neighbors.Count;
+
+        nextExit = neighbors[nextID];
+    }
 }
 
 /// <summary>
