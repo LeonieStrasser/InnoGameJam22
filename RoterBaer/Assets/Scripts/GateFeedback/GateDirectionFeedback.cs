8a32f358c4ccf92e2eb971688d2f84d18b925b74
Leonie Straßer <leonie.strasser@yahoo.de>
2022-11-12 13:22:35 +0100

    Gate Feedback Sprites

---
+++ RoterBaer/Assets/Scripts/GateFeedback/GateDirectionFeedback.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateDirectionFeedback : MonoBehaviour
+{
+    [SerializeField] GameObject directionSprite;
+    [SerializeField] float rotationSpeed;
+    
+    TrackGate myTrackGate;
+
+    private void Awake()
+    {
+        myTrackGate = GetComponent<TrackGate>();
+        myTrackGate.NextExitChangedEvent += RotateToNextExit;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       // myTrackpoint.NextExit
+    }
+
+    private void OnDestroy()
+    {
+        myTrackGate.NextExitChangedEvent -= RotateToNextExit;
+    }
+
+    private void Update()
+    {
+        RotateToNextExit();
+    }
+
+    void RotateToNextExit()
+    {
+        
+        directionSprite.transform.LookAt(myTrackGate.NextExit.transform);
+    }
+
+
+}
